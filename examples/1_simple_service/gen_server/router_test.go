67881a41accd6d16c1c4bc9d568ca83190ab4ed9
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-09-05 17:41:56 +0300

    Add router test for example 1

---
+++ examples/1_simple_service/gen_server/router_test.go
@@ -0,0 +1,58 @@
+package gen_server
+
+import (
+	"encoding/json"
+	"github.com/rwlist/gjrpc/examples/1_simple_service/proto"
+	"github.com/rwlist/gjrpc/pkg/jsonrpc"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type inventoryImpl struct {
+	foo func()
+	bar func()
+}
+
+func (i *inventoryImpl) Foo() (proto.Foo, error) {
+	i.foo()
+	return proto.Foo{}, nil
+}
+
+func (i *inventoryImpl) Bar(proto.Bar) error {
+	i.bar()
+	return nil
+}
+
+func TestRouter(t *testing.T) {
+	impl := &inventoryImpl{}
+	router := NewRouter(Handlers{Inventory: impl})
+
+	type testcase struct {
+		method  string
+		funcPtr *func()
+	}
+
+	cases := []testcase{
+		{
+			method:  "inventory.foo",
+			funcPtr: &impl.foo,
+		},
+		{
+			method:  "inventory.bar",
+			funcPtr: &impl.bar,
+		},
+	}
+	for _, test := range cases {
+		called := false
+		fun := func() { called = true }
+		*test.funcPtr = fun
+
+		router.Handle(&jsonrpc.Request{
+			Version: jsonrpc.Version,
+			Method:  test.method,
+			Params:  json.RawMessage("{}"),
+			ID:      jsonrpc.ID("0"),
+		})
+		assert.True(t, called)
+	}
+}
